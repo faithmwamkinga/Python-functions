358a2a9f6c46e0c8641384257d13a71d7f81b530
faithmwamkinga <faithmwamkinga@gmail.com>
2023-03-29 20:57:24 +0300

    functions-directory commit

---
+++ functions/hello.py
@@ -0,0 +1,8 @@
+def hello(name):
+    print(f"Hello{name}")
+
+def welcome(student,school):
+    print(f"Welcome to my {school},{student}")    
+
+
+    
---
+++ functions/math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a + b
+    return answer
+
+def subtraction(first,second):
+    answer1 = first - second
+    return answer1
+
+def division(third,fourth):
+    answer2 = third/fourth
+    return answer2
+
+def multiplication(fifth,sixth):
+    answer3 = fifth*sixth
+    return answer3
+
+def modulus(seventh,eighth):
+    answer4 = seventh%eighth
+    return answer4
